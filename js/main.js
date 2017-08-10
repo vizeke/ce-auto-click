4630eba5e1e3141569a8470eaf1de4ad94fa32f2
vizeke <vizeke@gmail.com>
2017-08-10 17:53:18 -0300

    add zone.js mockup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
+/*
+* This zone starts a timer at the start of each taskEnv,
+* and stops it at the end. It accumulates the total run
+* time internally, exposing it via `zone.time()`
+*
+* Note that this is the time the CPU is spending doing
+* bogosort, as opposed to the time from the start
+* of the algorithm until it's completion.
+*/
+/* const profilingZoneSpec = ( () => {
+    var time = 0,
+        // use the high-res timer if available
+        timer = performance ?
+            performance.now.bind( performance ) :
+            Date.now.bind( Date );
+    return {
+        onInvokeTask: ( delegate, current, target, task, applyThis, applyArgs ) => {
+            this.start = timer();
+            delegate.invokeTask( target, task, applyThis, applyArgs );
+            time += timer() - this.start;
+        },
+        time: () => Math.floor( time * 100 ) / 100 + 'ms',
+        reset: () => time = 0
+    };
+} )(); */
+
 if ( window.location.href.indexOf( 'https://www.kabum.com.br/produto' ) >= 0 ) {
-    const promotion = new Promotion()
-    promotion.mockParseProductList()
-    console.log( 'added' )
+
+    const main = () => new Promotion().mockParseProductList();
+
+    // Zone.current.fork( profilingZoneSpec ).run( main );
+    main();
 }
 if ( window.location.href.indexOf( 'https://www.kabum.com.br/cgi-local/site/carrinho' ) >= 0 ) {
     new Cart();
